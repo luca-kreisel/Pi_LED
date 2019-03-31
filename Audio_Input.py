b2948f67479f1d68be5f628054714cc494d5b7b8
Luca <Luca>
2019-04-01 01:47:41 +0200

    Code on Raspberry Pi: Microphone and LED working, but some issues

--- Audio_Input.py
+++ Audio_Input.py
@@ -1,14 +1,24 @@
 import pyaudio
 import numpy as np
 import colorsys
-import pygame
+from neopixel import *
 
 
 
 # Constants for Audio Input
-CHUNK = 1024*4
+CHUNK = 1024*2
 RATE = 44100
 FORMAT = pyaudio.paInt16
+
+# LED strip configuration:
+LED_COUNT      = 300     # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 # Setup pyaudio and stream
 p = pyaudio.PyAudio()
@@ -16,12 +26,14 @@ stream = p.open(format=FORMAT,
                 channels=1,
                 rate=RATE,
                 input=True)
+#Setup led strip
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip.begin()
 
-pygame.init()
-screen = pygame.display.set_mode((1000, 1000))
 while True:
     # Get pitch of input every 0.1s
-    data = stream.read(CHUNK)
+    data = stream.read(CHUNK,
+		exception_on_overflow = False)
 
     # convert to int array
     data_int = np.fromstring(data,np.int16)
@@ -34,19 +46,20 @@ while True:
 
     # map pitch to colour(HSV):
     # Range of pitch is usually [0,500], so calculate hue value (0 to 1) of Hsv as: pitch/500
-    hue = 1
-    if pitch < 1000:
-        hue =  pitch/1000
+    hue = 1.0
+    if pitch < 100:
+        hue =  pitch/100.0
     print(hue)
     # calculate (normalized) rgb value for easier output
     (r,g,b) = colorsys.hsv_to_rgb(hue,0.75,0.75)
 
     (r,g,b) = (r*255,g*255,b*255)
 
-    #output using pygame
+    #output using neopixel
+    for i in range(strip.numPixels()):
+            strip.setPixelColorRGB(i, int(r), int(g),int(b))
+    strip.show()
 
-    screen.fill((r,g,b))
-    pygame.display.update()
 
 
 
