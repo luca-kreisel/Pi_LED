9594dee106221efc7c6925e06f0a8f3b42180839
Luca Marius Kreisel <Luca Marius Kreisel>
2019-04-27 22:54:51 +0200

    Update LED_Server.py

--- LED_Server.py
+++ LED_Server.py
@@ -4,7 +4,7 @@ from neopixel import *
 
 
 # LED strip configuration:
-LED_COUNT      = 300*2    # Number of LED pixels.
+LED_COUNT      = 300*4    # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
