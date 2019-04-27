257c6ef7a14e55d6d3ba811dd1ac1076fe9c9536
Luca Marius Kreisel <Luca Marius Kreisel>
2019-04-27 23:44:34 +0200

    Adaptive Brightness

--- LED_Server.py
+++ LED_Server.py
@@ -9,7 +9,7 @@ LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 40     # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 45     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
@@ -48,6 +48,9 @@ while True:
             g = int(g_s)
         if b_s != '':
             b = int(b_s)
+        #Calculate brightness so no voltage drop on the led
+        relative = (r+g+b)/(3*255)
+        strip.setBrightness(relative*45)
 
         #display colour on strip
         for i in range(300):
