8fa2c52b42f24cb810e7042b71762c3bf55918cc
Luca Marius Kreisel <Luca Marius Kreisel>
2019-04-27 13:52:45 +0200

    Server Client Test

--- AmbiPi.py
+++ AmbiPi.py
@@ -1,23 +1,56 @@
 from PIL import ImageGrab
 import numpy as np
 
+import time
+import socket
+import pickle
+import  sys
 
 
 
-#Get Screenshot using ImageGrab and convert it to numpy array, concatinate it so it is only one continuous array of pixels
-screenshot = ImageGrab.grab()
-screenshot = screenshot.resize((100,100))
-screen_np = np.array(screenshot)
-(i,j,k) = screen_np.shape
-screen_np = np.reshape(screen_np,(i*j,k))
+#Constants
+WAIT = 0.05
+Pi_IP = "192.168.1.158"
+Pi_Port = 6000
+
+
+
+
+#Setup and connect socket
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((Pi_IP,Pi_Port))
+
+while True:
+    #Get Screenshot using ImageGrab and convert it to numpy array, concatinate it so it is only one continuous array of pixels
+    screenshot = ImageGrab.grab()
+    screenshot = screenshot.resize((150,150))
+    screen_np = np.array(screenshot)
+    (i,j,k) = screen_np.shape
+    screen_np = np.reshape(screen_np,(i*j,k))
 
 
 
 
 
-#Find the most frequent colour
-colours, counts = np.unique(screen_np, return_counts=True, axis= 0)
-most_frequent_colour = colours[np.argmax(counts)]
+    #Find the most frequent colour
+    colours, counts = np.unique(screen_np, return_counts=True, axis= 0)
+    most_frequent_colour = colours[np.argmax(counts)]
+    print(most_frequent_colour)
+    r = most_frequent_colour[0]
+    g = most_frequent_colour[1]
+    b = most_frequent_colour[2]
 
 
-print(most_frequent_colour)
+    #send data over tcp socket to Pi:
+
+    #marshall data first (resulting size 29)
+    data = pickle.dumps((r,g,b))
+    #send it over tcp
+    print (sys.getsizeof(data))
+    s.send(data)
+
+
+
+    time.sleep(WAIT)
+
+
---
+++ LED_Server.py
@@ -0,0 +1,22 @@
+import socket
+import pickle
+
+#Create and bind socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#So you can reuse server after restart more quickly
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(("",6000))
+
+s.listen(1)
+
+while True:
+    s_new, client = s.accept()
+    print (client)
+    while True:
+        data_recv = s_new.recv(160)
+
+        data = pickle.loads(data_recv)
+        print (data)
+
